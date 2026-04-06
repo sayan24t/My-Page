32e60315276b1f3d793eb853ea400c945f55b579
Sayan <sayan24t@gmail.com>
2026-04-06 18:37:40 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("cta-btn");
 
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("You clicked the button 🚀");
+    });
+  }
+});
